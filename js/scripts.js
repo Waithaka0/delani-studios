22b7f714cdb54d08b91da55ae2629db2369516bc
Waithaka Gitau <wgitau5@gmail.com>
2020-10-12 18:51:45 +0300

    background image added for welcome section

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,1 @@
-$(function(){
-  $('.portimg').hover(function(){
-        $('.porttext',this).slideToggle('slow');
-     }, function(){
-        $('.porttext',this).slideToggle('slow');
-     });
-}
+
