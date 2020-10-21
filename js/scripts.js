b64485499727f35db67218a407d6d929faa5431a
Waithaka Gitau <wgitau5@gmail.com>
2020-10-21 11:38:52 +0300

    added functionalities

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
-$(document).ready(function() {
-  $("#clickable-dev").click(function(){
-     $("#dev-info-hidden").toggle();
-     $("#dev-info-showing").toggle();
- });
+$(document).ready(function(){
+    $('.image-frame').hover(function(){
+            $('.image-caption', this).slideToggle('slow');
+        }, function(){
+            $('.image-caption', this).slideToggle('slow');
+    });
 
+    $(".clickable-design").click(function(){
+        $(".design-info-hidden").toggle();
+        $(".design-info-showing").toggle();
+    });
 
-  $("form#form").on('submit',function(event){
-            event.preventDefault();
-            let name = $("input#name").val();
-            let email = $("input#email").val();
-            let message = $("textarea#message").val();
+    $(".clickable-prod").click(function(){
+        $(".prod-info-hidden").toggle();
+        $(".prod-info-showing").toggle();
+    });
 
-            if ($("input#name").val() && $("input#email").val()){
-                alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
-            }
-            else {
-                alert("Please provide your correct name and email!");
-            }
-        });
- 
+    $(".clickable-dev").click(function(){
+        $(".dev-info-hidden").toggle();
+        $(".dev-info-showing").toggle();
+    });
+
+    $("form#form").on('submit',function(event){
+        event.preventDefault();
+        let name = $("input#name").val();
+        let email = $("input#email").val();
+        let message = $("textarea#message").val();
+
+        if ($("input#name").val() && $("input#email").val()){
+            alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you soon!");
+        }
+        else {
+            alert("Please provide a valid name and email!");
+        };
+    });
+})
